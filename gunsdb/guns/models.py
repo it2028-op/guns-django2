40751c732a460bee8faaf025dd28338fa8aeaebc
it2028 <76162655+it2028-op@users.noreply.github.com>
2023-06-16 14:45:35 +0200

    1.0.2 - finální oprava commitu

--- gunsdb/guns/models.py
+++ gunsdb/guns/models.py
@@ -1,10 +1,5 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-from django.urls import reverse
-
-
-def meadia_attachment_path(instance, filename):
-    return "film/" + str(instance.gun.id) + "/attachments/" + filename
 
 
 class Type(models.Model):
@@ -22,7 +17,7 @@ class Type(models.Model):
 class Maker(models.Model):
     name = models.CharField(max_length=100, unique=True, verbose_name="Název firmy/výrobce",
                             help_text="Zadejte název výrobce nebo firmy")
-    address = models.CharField(max_length=150, verbose_name="Adresa firmy/výrobce",
+    address = models.CharField(blank=True, max_length=150, verbose_name="Adresa firmy/výrobce",
                                help_text='Zadejte adresu firmy nebo výrobce, pokud je známá.')
     about = models.TextField(max_length=300, blank=True, null=True,
                              verbose_name="O vyrábějící firmě nebo výrobci zbraně")
@@ -32,6 +27,22 @@ class Maker(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Inventor(models.Model):
+    name = models.CharField(max_length=80, verbose_name='Jméno vynálezce', help_text='Zadejte jméno vynálezce', )
+    surname = models.CharField(max_length=50, verbose_name='Příjmení vynálezce', help_text='Zadejte příjmení vynálezce')
+    birth = models.DateField(blank=True, null=True, verbose_name='Datum narození')
+    death = models.DateField(blank=True, null=True, verbose_name='Datum úmrtí')
+    about = models.TextField(blank=True, verbose_name='O vynálezci')
+
+    class Meta:
+        ordering = ['surname', 'name']
+        verbose_name = 'Vynálezce'
+        verbose_name_plural = 'Vynálezci'
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
 
 
 class Gun(models.Model):
@@ -48,11 +59,15 @@ class Gun(models.Model):
                                help_text="Zadejte hmotnost zbraně v gramech")
     magazine = models.IntegerField(verbose_name="Celková kapacita obvyklého zásobníku",
                                    help_text="Zadejte množtví munice v základním zásobníku zbraně", default=1)
-    description = models.TextField(max_length=300, blank=True, verbose_name="Popis zbraně",
+    '''developed = models.PositiveIntegerField(blank=True, null=True,
+                                             validators=[MinValueValidator(900), MaxValueValidator(2100)],
+                                             verbose_name='Rok vydání', help_text='Zadejte rok vydání (1500 - 2100)')'''
+    description = models.TextField(blank=True, verbose_name="Popis zbraně",
                                    help_text="Popište tuto zbraň (historie, vlastnosti"
                                              ", zvláštnosti...)")
     type = models.ManyToManyField(Type, help_text='Vyberte typ zbraně')
-    maker = models.ManyToManyField(Maker, help_text='Vyberte výrobce zbraně')
+    maker = models.ManyToManyField(Maker, blank=True, help_text='Vyberte výrobce zbraně')
+    inventor = models.ManyToManyField(Inventor, blank=True, help_text='Vyberte vynálezces zbraně')
 
     rate = models.FloatField(default=5.0,
                              validators=[MinValueValidator(1.0), MaxValueValidator(10.0)],
@@ -68,23 +83,3 @@ class Gun(models.Model):
 
     def get_detail_url(self):
         return reverse('gun-detail', arg=[str(self.id)])
-
-
-class Attachment(models.Model):
-    title = models.CharField(max_length=300, verbose_name='Titul obrázku')
-    last_update = models.DateTimeField(auto_now=True)
-    file = models.FileField(upload_to=meadia_attachment_path, null=True, verbose_name="Soubor")
-    TYPE_OF_ATTACHMENT = (
-        ('image', 'Image'),
-        ('video', 'Video')
-    )
-    type = models.CharField(max_length=5, choices=TYPE_OF_ATTACHMENT, blank=True,
-                            default='image', help_text='Vyberte typ přílohy(obrázek nebo video)',
-                            verbose_name="Typ přílohy")
-    gun = models.ForeignKey(Gun, on_delete=models.CASCADE)
-
-    class Meta:
-        ordering = ["-last_update", "type"]
-
-        def __str__(self):
-            return f"{self.title} - {self.type} - {str(self.last_update)}"
