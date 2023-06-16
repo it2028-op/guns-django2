53bc7efe087ca24c2645899510d4158688c017ff
it2028 <76162655+it2028-op@users.noreply.github.com>
2023-06-16 14:30:34 +0200

    1.0.1 - oprava chyby commitu

--- gunsdb/guns/admin.py
+++ gunsdb/guns/admin.py
@@ -4,4 +4,5 @@ from .models import *
 # Register your models here.
 admin.site.register(Type)
 admin.site.register(Maker)
+admin.site.register(Inventor)
 admin.site.register(Gun)
--- gunsdb/guns/urls.py
+++ gunsdb/guns/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
+    path('guns/', views.GunListView.as_view(), name='guns'),
+    path('guns/<int:pk>', views.GunDetailView.as_view(), name='gun_detail'),
 ]
