c40917e6724e8afd2ab3665624f82d8a31aaeb3f
it2028-op <svanus@seznam.cz>
2023-06-16 14:36:30 +0200

    Revert "1.0 - vizualizace"
    This reverts commit 0f8e23e808346484fdd4c48dadba95e2d31ecea3.

--- guns-django/gunsdb/guns/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('guns/', views.GunListView.as_view(), name='guns'),
-    path('guns/<int:pk>', views.GunDetailView.as_view(), name='gun_detail'),
-]
